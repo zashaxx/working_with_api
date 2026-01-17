d17a35fecfee724c0ab5eb60316e75223d2d8e81
aayushkoirala8848-boop <aayushkoirala8848@gmail.com>
2026-01-17 20:08:55 +0545

    learn about PUT and DELETE request

--- fast-api/books.py
+++ fast-api/books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query ,Body
+from fastapi import FastAPI, Query, Body
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -82,7 +82,22 @@ async def read_author_category_by_query(book_author: str, category: str):
             books_to_return.append(book)
     return books_to_return
 
+
 @app.post("/books/create_book")
-async def create_new_book(new_book = Body()):
+async def create_new_book(new_book=Body()):
     BOOKS.append(new_book)
-    
+
+@app.put("/books/update_book")
+async def update_book(updated_book = Body()):
+    for i in range (len(BOOKS)):
+        if BOOKS[i].get("title").casefold() == updated_book.get("title").casefold():
+            BOOKS[i] = updated_book
+            return {"message": "Book updated successfully"}
+        
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range (len(BOOKS)):
+        if BOOKS[i].get("title").casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            return {"message": "Book deleted successfully"}
+    return {"message": "Book not found"}
