1c50c319ec69b955a602a9a7a9cf73f69af0315a
zashaxx <hisenberg2248@gmail.com>
2026-01-20 20:55:50 +0545

    make a new db table for storing user login details and used bcrypt for hashing the password

--- fast-api/Todo_app/routers/auth.py
+++ fast-api/Todo_app/routers/auth.py
@@ -1,9 +1,56 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from models import Users
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from database import SessionLocal
+from typing import Annotated
+from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm
+
 
 
 router = APIRouter()
 
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-@router.get("/auth/")
-async def get_user():
-    return {"user": "authenticated"}
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    firstname: str
+    lastname: str
+    password: str
+    role: str
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@router.post("/auth/", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.firstname,
+        last_name=create_user_request.lastname,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
+        is_active=True,
+        role=create_user_request.role,
+    )
+
+    db.add(create_user_model)
+    db.commit()
+
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    
+    
