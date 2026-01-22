8447066b39a8d2e6c738c929154a702ff18116fa
zashaxx <hisenberg2248@gmail.com>
2026-01-22 12:06:50 +0545

    learn to work with mysql database as well

--- fast-api/Todo_app/database.py
+++ fast-api/Todo_app/database.py
@@ -2,9 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+# SQLALCHEMY_DATABASE_URL = (
+#     "postgresql://postgres:test1234!@localhost:5432/TodoApplicationServer"
+# )
+
 SQLALCHEMY_DATABASE_URL = (
-    "postgresql://postgres:test1234!@localhost:5432/TodoApplicationServer"
+    "mysql+pymysql://root:test1234!@localhost:3306/TodoApplicationServer"
 )
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 
