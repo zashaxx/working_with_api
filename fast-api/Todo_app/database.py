54cd212e44f49ed52d5ee0864ff4daed8c6e6179
zashaxx <hisenberg2248@gmail.com>
2026-01-22 10:52:16 +0545

    Update database connection string with postgresql.

--- fast-api/Todo_app/database.py
+++ fast-api/Todo_app/database.py
@@ -2,12 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todoapp.db"
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+SQLALCHEMY_DATABASE_URL = (
+    "postgresql://postgres:test1234!@localhost:5432/TodoApplicationServer"
 )
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
